f11504822b9c1ef108eec2c2ec74a73f5e3b93a6
bibek_thapa <bibekthapa922@gmail.com>
2022-11-08 14:22:15 +0545

    add slug in category

--- src/models/CategoryModel.ts
+++ src/models/CategoryModel.ts
@@ -1,16 +1,31 @@
 import mongoose, { Schema } from 'mongoose';
+import slugify from 'slugify';
 
 export interface ICategory extends Document {
   _id: string;
   title: string;
   description: string;
+  slug: string;
   imageLink?: string;
 }
 
 const CategorySchema: Schema = new Schema({
   title: { type: 'string', required: true },
   description: { type: 'string', required: true },
+  slug: { type: 'string', slug: 'title', unique: true },
   imageLink: { type: 'string', required: false },
+});
+
+CategorySchema.set('toJSON', {
+  transform: function (doc, ret, options) {
+    delete ret.__v;
+  },
+});
+
+CategorySchema.pre('save', function (next) {
+  if (!(this.isModified('title') || this.isNew)) return next();
+  this.slug = `${slugify(this.title, { lower: true })}`;
+  next();
 });
 
 export default mongoose.model<ICategory>('Category', CategorySchema);
