324d30ac9d931ffb4b6912fcfa899960f4f7ca46
bibek_thapa <bibekthapa922@gmail.com>
2022-11-08 14:21:00 +0545

    add user update api

---
+++ src/controllers/userController.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from 'express';
+import Joi from 'joi';
+import NotFoundError from '../errors/notFound';
+import ValidationError from '../errors/validation';
+import User from 'src/models/UserModel';
+
+async function updateUser(req: Request, res: Response, next: NextFunction) {
+  try {
+    const userId = req.params.userId;
+    let { error, value } = Joi.object({
+      _id: Joi.string().required(),
+      name: Joi.string(),
+      role: Joi.string(),
+      imageLink: Joi.string().allow(null),
+      releventCategories: Joi.array().items(Joi.string()),
+    }).validate({ ...req.body, _id: userId });
+
+    if (error) return next(new ValidationError(error.details[0].message));
+
+    let user = await User.findOneAndUpdate({ _id: value._id }, value, { new: true });
+    if (!user) return next(new NotFoundError('user not found'));
+
+    return res.status(200).send({ message: 'user updated successfully.', user });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export default {
+  updateUser,
+};
