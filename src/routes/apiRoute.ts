f6cfca95e8dcf3b94e2668abd661582421627b61
bibek_thapa <bibekthapa922@gmail.com>
2022-11-08 14:23:27 +0545

    update api route

--- src/routes/apiRoute.ts
+++ src/routes/apiRoute.ts
@@ -3,12 +3,14 @@ import { checkJwt } from 'src/middlewares/jwt';
 import AuthRoute from 'src/routes/authRoute';
 import CategoryRoute from 'src/routes/categoryRoute';
 import PostRoute from 'src/routes/postRoute';
+import UserRoute from 'src/routes/userRoute';
 
 const router = Router();
 
 router.use('/auth', AuthRoute);
 router.use('/category', CategoryRoute);
 router.use('/post', PostRoute);
+router.use('/user', UserRoute);
 
 router.get('/', (req, res) => res.status(200).send('<h1>Sojo News App Api</h1>'));
 
