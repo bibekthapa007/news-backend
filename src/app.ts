ad6594dbdb3d816c56f2e037c018b66bc1527866
bibek_thapa <bibekthapa922@gmail.com>
2022-11-12 22:33:19 +0545

    add cookieparser

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import helmet from 'helmet';
 import multer from 'multer';
+import cookieParser from 'cookie-parser';
 
 import ApiRoute from './routes/apiRoute';
 import { errorHandler } from './middlewares/errorhandler';
@@ -11,11 +12,13 @@ import { errorHandler } from './middlewares/errorhandler';
 const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(cookieParser());
 
 app.use(helmet());
 app.use(
   cors({
     credentials: true,
+    origin: ['http://localhost:3000'],
   }),
 );
 app.disable('x-powered-by');
